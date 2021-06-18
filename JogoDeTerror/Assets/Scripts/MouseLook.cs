962795af90574c709ca3c48ebf8651a07b130e0a
DrakenXD <79544585+DrakenXD@users.noreply.github.com>
2021-06-17 23:23:58 -0300

    AT

--- JogoDeTerror/Assets/Scripts/MouseLook.cs
+++ JogoDeTerror/Assets/Scripts/MouseLook.cs
@@ -6,18 +6,23 @@ public class MouseLook : MonoBehaviour
 {
     [Range(0.01f,10f)]public float mouseSensivity = 2f;
 
+    public Transform pointTransform;
+    public Camera mainCamera;
     public Transform playerbody;
+    public LayerMask layerHit;
 
     float xRotation = 0;
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float mouseX = Input.GetAxis("Mouse X") * mouseSensivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity;
 
@@ -28,5 +33,43 @@ public class MouseLook : MonoBehaviour
        
         playerbody.Rotate(Vector3.up*mouseX);
 
+        Aim();
     }
+
+    private  void Aim()
+    {
+        Ray cameraray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        Plane groundplane = new Plane(Vector3.up, Vector3.zero);
+
+
+
+        if (Physics.Raycast(cameraray, out RaycastHit raycast, Mathf.Infinity, layerHit))
+        {
+
+            Vector3 pointToLook = cameraray.GetPoint(raycast.distance);
+
+            Debug.DrawLine(cameraray.origin, pointToLook, Color.blue);
+
+            pointTransform.localPosition = pointToLook;
+
+
+        }
+
+
+    }
+
+    /*
+    private (bool success,Vector3 position) GetMousePosition()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray,out var hitinfo, Mathf.Infinity,layerHit))
+        {
+            return (success: true, position:hitinfo.point);
+        }else
+        {
+            return (success: false, position: Vector3.zero);
+        }
+    }*/
 }
